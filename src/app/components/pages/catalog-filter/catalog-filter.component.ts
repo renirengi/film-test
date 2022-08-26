449e4709f2462d46fddf72c5b198454f7f067cc7
Olga <TomieForever@gmail.com>
2022-08-26 16:01:04 +0400

    fix all mistakes

--- src/app/components/pages/catalog-filter/catalog-filter.component.ts
+++ src/app/components/pages/catalog-filter/catalog-filter.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { map, Observable, Subscription, tap } from 'rxjs';
 import { FilmService } from 'src/app/services/film.service';
@@ -7,7 +13,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-catalog-filter',
   templateUrl: './catalog-filter.component.html',
-  styleUrls: ['./catalog-filter.component.scss']
+  styleUrls: ['./catalog-filter.component.scss'],
 })
 export class CatalogFilterComponent implements OnInit, OnDestroy {
   @Output() filtersChanged = new EventEmitter<{ [key: string]: string }>();
@@ -22,11 +28,11 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
     languages: new FormControl(['']),
     rated: new FormControl(['']),
     year: new FormControl(['']),
-    priceMax: new FormControl(['']),
-    priceMin: new FormControl(['']),
+    priceMax: new FormControl(''),
+    priceMin: new FormControl(''),
     onlySale: new FormControl(false),
     price: new FormControl(['']),
-    q: new FormControl('')
+    q: new FormControl(''),
   });
 
   public genres$: Observable<string[]>;
@@ -45,17 +51,13 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
 
   public prices$: Observable<string[]>;
 
-  public prices:string[]=[];
+  public prices: string[] = [];
 
-  public currentGenre: string | null ='';
+  public currentGenre: string | null = '';
 
-  selected: string =''
+  selected: string = '';
 
-  constructor(
-    private filmService: FilmService,
-    private route: ActivatedRoute
-  ) {
-
+  constructor(private filmService: FilmService, private route: ActivatedRoute) {
     this.genres$ = this.filmService.getAvailable('genre');
     this.types$ = this.filmService.getAvailable('type');
     this.directors$ = this.filmService.getAvailable('director');
@@ -64,102 +66,113 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
     this.rateds$ = this.filmService.getAvailable('rated');
     this.years$ = this.filmService.getAvailable('years');
     this.prices$ = this.filmService.getAvailable('prices');
-    this.filmService.getAvailable('prices').subscribe({next:(data) => {
-      this.prices=data;
-      console.log(this.prices);
-    }});
-    console.log(this.prices);
   }
 
-   public ngOnInit() {
-    const searchString$ = this.route.queryParams.pipe(map(params => params['q'])).pipe(
-      tap((q) => {
-        this.filtersForm.patchValue({q});
-        this.filmService.filmSearchString = q || '';
-        this.onChange();
-      })
-    );
+  public async ngOnInit() {
+    await this.getPrices().then((prices) => (this.prices = prices));
+    this.filtersForm.get('priceMin')!.patchValue(this.prices[0]);
+    this.filtersForm
+      .get('priceMax')!
+      .patchValue(this.prices[this.prices.length - 1]);
+    const searchString$ = this.route.queryParams
+      .pipe(map((params) => params['q']))
+      .pipe(
+        tap((q) => {
+          this.filtersForm.patchValue({ q });
+          this.filmService.filmSearchString = q || '';
+          this.onChange();
+        })
+      );
 
     this.currentGenre = this.filmService._currentGenre$.getValue();
     if (this.currentGenre) {
-      let newArr=[]
-      newArr.push(this.currentGenre)
+      let newArr = [];
+      newArr.push(this.currentGenre);
       this.filtersForm.get('genres')!.patchValue(newArr);
       this.onChange();
     }
 
     this.subscription.add(searchString$.subscribe());
-    }
-
+  }
 
   public ngOnDestroy(): void {
     this.subscription.unsubscribe();
-}
+  }
 
-public onChange() {
-  let filterParams: {} = {};
+  public getPrices(): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      let prices: string[] = [];
+      this.filmService.getAvailable('prices').subscribe((data) => {
+        data.forEach((price) => {
+          prices.push(price);
+        });
+        resolve(prices);
+      });
+    });
+  }
 
-  filterParams = Object.entries(this.filtersForm.value).reduce((acc, [key, value]) => {
-    let keyString: string;
-    let valueString: string;
+  public onChange() {
+    let filterParams: {} = {};
+    filterParams = Object.entries(this.filtersForm.value).reduce(
+      (acc, [key, value]) => {
+        let keyString: string;
+        let valueString: string;
 
-    if (key=='priceMin' || key=='priceMax') {
-     let priceMinArr = [];
-     let priceMaxArr = [];
-     let minValue:number = +'5.6';
-     this.filmService.getAvailable('prices').subscribe({next:(data) => {
-      this.prices=data;
-      minValue=+this.prices[0];
-    }});
-    console.log(minValue);
-     //let maxValue = this.prices[this.prices.length-1]
-     //console.log(this.prices[0])
-      if(key=='priceMin'){
-        //minValue = this.filtersForm.value['priceMin'];
-        console.log(this.filtersForm.value['priceMin'])
-        //priceMinArr.push(String(minValue));
+        if (key == 'priceMin' || key == 'priceMax') {
+          let price = [''];
+          let minValue;
+          let maxValue;
+          if (
+            this.filtersForm.value['priceMax'] &&
+            this.filtersForm.value['priceMin']
+          ) {
+            minValue = +this.filtersForm.value['priceMin'];
+            maxValue = +this.filtersForm.value['priceMax'];
+            price = this.getInterval(minValue, maxValue);
+            keyString = `price_like`;
+            valueString = price.reduce(
+              (acc, val, i) => (i === 0 ? `(${val})` : `${acc}|(${val})`),
+              ''
+            );
+            console.log(valueString);
+            if (valueString !== '()') {
+              acc = { ...acc, [keyString]: valueString };
+            }
+          }
+        } else if (Array.isArray(value) && value.length > 0) {
+          keyString = `${key}_like`;
+          valueString = value.reduce(
+            (acc, val, i) => (i === 0 ? `(${val})` : `${acc}|(${val})`),
+            ''
+          );
 
-      }
-        else{
-          //priceMax=+value;
+          if (valueString !== '()') {
+            acc = { ...acc, [keyString]: valueString };
+          }
+        } else if (typeof value === 'boolean' && value === true) {
+          [keyString, valueString] = this.booleanFilterGenerator(key);
+          acc = { ...acc, [keyString]: valueString };
         }
-        //this.getInterval(priceMin, priceMax);
 
-      keyString = key;
-      //valueString = value;
+        return acc;
+      },
+      {}
+    );
 
-      ///acc = {...acc, [keyString]: valueString};
-    } else if (Array.isArray(value) && value.length > 0) {
-      keyString = `${key}_like`;
-      valueString = value.reduce((acc, val, i) => i === 0 ? `(${val})`: `${acc}|(${val})`, '');
+    this.filtersChanged.emit(filterParams);
+  }
 
-      if(valueString!=='()'){
-      acc = {...acc, [keyString]: valueString};
-    }
+  private getInterval(min: any, max: any) {
+    return this.prices
+      .filter((el) => el >= min && el <= max)
+      .map((el) => String(el));
+  }
 
-    } else if (typeof value === 'boolean' && value === true) {
-      [keyString, valueString] = this.booleanFilterGenerator(key);
-      acc = {...acc, [keyString]: valueString};
-    }
+  private booleanFilterGenerator(key: string): string[] {
+    const config: { [key: string]: () => string[] } = {
+      notReissue: () => ['reissue', 'false'],
+    };
 
-    return acc;
-  }, {});
-  //console.log(filterParams)
-
-  this.filtersChanged.emit(filterParams);
-}
-
-private getInterval(min:any, max:any){
-return this.prices.filter((el) => el >= min && el<= max).map((el)=>String(el))
-}
-
-private booleanFilterGenerator(key: string): string[] {
-  const config: {[key: string]: () => string[]} = {
-    notReissue: () => ['reissue', 'false'],
-  };
-
-  return config[key]();
-}
-
-
+    return config[key]();
+  }
 }
