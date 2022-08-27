d5376668c6c9fe5ff1904968ac5f93859ee63640
Olga <TomieForever@gmail.com>
2022-08-27 17:09:24 +0400

    add data.db

--- src/app/components/pages/catalog-filter/catalog-filter.component.ts
+++ src/app/components/pages/catalog-filter/catalog-filter.component.ts
@@ -6,7 +6,7 @@ import {
   Output,
 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { map, Observable, Subscription, tap } from 'rxjs';
+import { forkJoin, map, Observable, Subscription, tap } from 'rxjs';
 import { FilmService } from 'src/app/services/film.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -19,81 +19,54 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
   @Output() filtersChanged = new EventEmitter<{ [key: string]: string }>();
 
   private subscription: Subscription = new Subscription();
-
   public filtersForm = new FormGroup({
-    genres: new FormControl(['']),
-    types: new FormControl(['']),
-    directors: new FormControl(['']),
-    countries: new FormControl(['']),
-    languages: new FormControl(['']),
-    rated: new FormControl(['']),
-    year: new FormControl(['']),
-    priceMax: new FormControl(''),
-    priceMin: new FormControl(''),
+    genres: new FormControl([]),
+    types: new FormControl([]),
+    directors: new FormControl([]),
+    countries: new FormControl([]),
+    languages: new FormControl([]),
+    rated: new FormControl([]),
+    year: new FormControl([]),
+    priceMax: new FormControl(),
+    priceMin: new FormControl(),
     onlySale: new FormControl(false),
-    price: new FormControl(['']),
+    price: new FormControl([]),
     q: new FormControl(''),
   });
-
-  public genres$: Observable<string[]>;
-
-  public types$: Observable<string[]>;
-
-  public directors$: Observable<string[]>;
-
-  public countries$: Observable<string[]>;
-
-  public languages$: Observable<string[]>;
-
-  public rateds$: Observable<string[]>;
-
-  public years$: Observable<string[]>;
-
-  public prices$: Observable<string[]>;
+  public filtersOptions$: Observable<{[key: string]: string[]}>
 
   public prices: string[] = [];
-
   public currentGenre: string | null = '';
-
   public searchString: string = '';
 
   selected: string = '';
 
   constructor(private filmService: FilmService, private route: ActivatedRoute) {
-    this.genres$ = this.filmService.getAvailable('genre');
-    this.types$ = this.filmService.getAvailable('type');
-    this.directors$ = this.filmService.getAvailable('director');
-    this.countries$ = this.filmService.getAvailable('country');
-    this.languages$ = this.filmService.getAvailable('language');
-    this.rateds$ = this.filmService.getAvailable('rated');
-    this.years$ = this.filmService.getAvailable('years');
-    this.prices$ = this.filmService.getAvailable('prices');
+    this.filtersOptions$ = forkJoin({
+      genres: this.filmService.getAvailable('genre'),
+      types: this.filmService.getAvailable('type'),
+      directors: this.filmService.getAvailable('director'),
+      countries: this.filmService.getAvailable('country'),
+      languages: this.filmService.getAvailable('language'),
+      rateds: this.filmService.getAvailable('rated'),
+      years: this.filmService.getAvailable('years'),
+      prices: this.filmService.getAvailable('prices'),
+    });
+
+
   }
 
   public ngOnInit() {
-    this.getPrices().then((prices) => (this.prices = prices));
-    this.filtersForm.get('priceMin')!.patchValue(this.prices[0]);
-    this.filtersForm
-      .get('priceMax')!
-      .patchValue(this.prices[this.prices.length - 1]);
     const searchString$ = this.route.queryParams
       .pipe(map((params) => params['q']))
       .pipe(
         tap((q) => {
           this.filtersForm.patchValue({ q });
-          //this.filmService.filmSearchString = q || '';
-          this.searchString= q;
+          this.filmService.filmSearchString = q || '';
+          this.searchString = q;
           this.onChange();
         })
       );
-
-    this.currentGenre = this.filmService._currentGenre$.getValue();
-    if (this.currentGenre) {
-      let newArr = [];
-      newArr.push(this.currentGenre);
-      this.filtersForm.get('genres')!.patchValue(newArr);
-      this.onChange();
-    }
 
     this.subscription.add(searchString$.subscribe());
   }
@@ -115,8 +88,8 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
   }
 
   public onChange() {
-    console.log (this.searchString)
-    let filterParams: { [key: string]: string} = {};
+    let filterParams: { [key: string]: string } = {};
+
     filterParams = Object.entries(this.filtersForm.value).reduce(
       (acc, [key, value]) => {
         let keyString: string;
@@ -138,9 +111,8 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
               (acc, val, i) => (i === 0 ? `(${val})` : `${acc}|(${val})`),
               ''
             );
-           if (valueString !== '()') {
-              acc = { ...acc, [keyString]: valueString };
-            }
+
+            acc = { ...acc, [keyString]: valueString };
           }
         } else if (Array.isArray(value) && value.length > 0) {
           keyString = `${key}_like`;
@@ -149,21 +121,18 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
             ''
           );
 
-          if (valueString !== '()') {
             acc = { ...acc, [keyString]: valueString };
-          }
 
         } else if (typeof value === 'boolean' && value === true) {
           [keyString, valueString] = this.booleanFilterGenerator(key);
           acc = { ...acc, [keyString]: valueString };
         }
-
         return acc;
       },
       {}
     );
     if (this.searchString) {
-      filterParams['q']=this.searchString;
+      filterParams['q'] = this.searchString;
     }
     this.filtersChanged.emit(filterParams);
 
@@ -177,7 +146,7 @@ export class CatalogFilterComponent implements OnInit, OnDestroy {
 
   private booleanFilterGenerator(key: string): string[] {
     const config: { [key: string]: () => string[] } = {
-      notReissue: () => ['reissue', 'false'],
+      onlySale: () => ['onlySale', 'false'],
     };
 
     return config[key]();
